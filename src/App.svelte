a55dedf7ada0a66e4ba68c280202b12c086cff10
jhaenen <jasper@haenenweb.nl>
2022-06-29 13:44:00 +0200

    Add tailwind

--- src/App.svelte
+++ src/App.svelte
@@ -3,9 +3,9 @@
   import Counter from './lib/Counter.svelte'
 </script>
 
-<main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello Typescript!</h1>
+<main class="text-center mx-0 my-auto p-4">
+  <img class="h-64 w-64" src={logo} alt="Svelte Logo" />
+  <h1 class="uppercase text-7xl font-thin m-8 max-w-xs">Hello Typescript!</h1>
 
   <Counter />
 
@@ -26,31 +26,12 @@
       Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
 
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
   h1 {
     color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
   }
 
   p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
+    @apply max-w-xs m-4 leading-snug;
   }
 
   @media (min-width: 480px) {
