7a171924f06aa3908f3feb33710dbae5d2f72807
jhaenen <jasper@haenenweb.nl>
2022-06-29 20:18:01 +0200

    Implement router

--- src/App.svelte
+++ src/App.svelte
@@ -1,46 +1,17 @@
 <script lang="ts">
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+    import page from 'page';
+
+    import Home from '@/routes/home.svelte';
+    import Results from '@/routes/results.svelte';
+
+    let current = Home;
+
+    page('/', () => {current = Home;});
+    page('/results', () => {current = Results;});
+
+    page.start();
 </script>
 
-<main class="text-center mx-0 my-auto p-4">
-  <img class="h-64 w-64" src={logo} alt="Svelte Logo" />
-  <h1 class="uppercase text-7xl font-thin m-8 max-w-xs">Hello Typescript!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+<main>
+    <svelte:component this={current} />
 </main>
-
-<style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  h1 {
-    color: #ff3e00;
-  }
-
-  p {
-    @apply max-w-xs m-4 leading-snug;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
-</style>
---
+++ src/routes/home.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+
+</script>
+
+<main>
+    <h1 class="font-thin text-5xl">DAS toernooi</h1>
+
+    <a href="/results">Resultaten</a>
+</main>
