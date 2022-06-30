24556876f54cdd710af7a3f8d4f8369f38f497d3
jhaenen <jasper@haenenweb.nl>
2022-06-30 14:24:38 +0200

    Bouncy loader

---
+++ src/view/load-ball.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import ball from "@/assets/basketball.svg";
+
+</script>
+
+<template>
+    <div class="flex flex-col items-center justify-end h-[75px] w-fit">
+        <img class="animate-bounce h-10 w-10" src={ball} alt=""/>
+        <span>Laden...</span>
+    </div>
+</template>
