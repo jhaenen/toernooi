b13a7977bf41af0ca4ae7c4eaa7722e871dc0371
jhaenen <jasper@haenenweb.nl>
2022-07-02 21:35:21 +0200

    Copy server to dist on build

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,26 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { resolve } from 'path'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+
+function resolveDir(relativePath: string) {
+  return resolve(__dirname, relativePath);
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    viteStaticCopy({
+      targets: [
+        { src: './server/php/*.php', dest: 'server' },
+        { src: './server/php/prod/env.ini', dest: 'server' },
+      ]
+    }),
+  ],
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      '@': resolveDir('./src'),
     },
   },
 })
