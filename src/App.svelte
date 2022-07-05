1ce920625bb8fad45a100b10e9213e3a713d6d7f
jhaenen <jasper@haenenweb.nl>
2022-07-05 15:03:38 +0200

    Fix navbar style

--- src/App.svelte
+++ src/App.svelte
@@ -32,10 +32,10 @@
     <div class="fixed bottom-0 w-full m-0 p-0 sm:top-0 sm:bottom-auto z-10">
         <nav class="flex h-[82px] justify-evenly flex-wrap sm:justify-start bg-primary text-white py-4 sm:p3">
             <a href="#/" class="hidden h-12 w-12 mx-4 sm:block"><img src={logo} alt="Svelte Logo"/></a>
-            <a href="#/" class="w-24 sm:py-3 sm:px-4"><NavButton label="Home" icon={home_icon} color={icon_color}/></a>
-            <a href="#/poules" class="w-24 sm:py-3 sm:px-4"><NavButton label="Poules" icon={poules_icon} color={icon_color}/></a>
-            <a href="#/schema" class="w-24 sm:py-3 sm:px-4"><NavButton label="Dagschema" icon={schedule_icon} color={icon_color}/></a>
-            <a href="#/org" class="w-24 sm:py-3 sm:px-4 hidden mi:block "><NavButton label="Organizatie" icon={org_icon} color={icon_color}/></a>
+            <a href="#/" class="w-24 sm:py-3 sm:px-4 sm:w-auto"><NavButton label="Home" icon={home_icon} color={icon_color}/></a>
+            <a href="#/poules" class="w-24 sm:py-3 sm:px-4 sm:w-auto"><NavButton label="Poules" icon={poules_icon} color={icon_color}/></a>
+            <a href="#/schema" class="w-24 sm:py-3 sm:px-4 sm:w-auto"><NavButton label="Dagschema" icon={schedule_icon} color={icon_color}/></a>
+            <a href="#/org" class="w-24 sm:py-3 sm:px-4 hidden mi:block sm:w-auto"><NavButton label="Organizatie" icon={org_icon} color={icon_color}/></a>
         </nav>
     </div>
     
