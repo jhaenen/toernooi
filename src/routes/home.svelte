64e5e61cf405175cb7a39bfbe337e83f0530f92c
jhaenen <jasper@haenenweb.nl>
2022-08-27 02:12:16 +0200

    Add content to homepage

--- src/routes/home.svelte
+++ src/routes/home.svelte
@@ -12,8 +12,35 @@
         <img class="w-[80%] h-auto max-w-xs sm:hidden" src={logo} alt="DAS logo" width="320" height="320"/>
 
         <!-- Main text -->
-        <div class="m-3">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam purus diam, gravida vel odio at, venenatis luctus libero. Morbi sed sem eu ante interdum lacinia in quis eros. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed id feugiat odio, vitae maximus quam. Sed suscipit ac dui eu tincidunt. Quisque lobortis rhoncus nulla, nec blandit justo dictum non. Pellentesque sit amet vehicula metus. Nunc vitae tempor odio. Donec vitae lacus sed eros pulvinar mattis sed non tellus. Quisque non lobortis nisl, vitae ultricies eros. Maecenas non lorem suscipit, suscipit erat in, molestie lectus. Vestibulum rutrum vitae odio eu suscipit. Vestibulum ultricies lacus vel nulla consectetur tempor. </p>
+        <div class="flex flex-col m-3 gap-4 max-w-6xl">
+            <p> 
+                Het is zover! Op zaterdag 10 september 2022 is het DAS familietoernooi om het eerste seizoen in de nieuwe Korba sporthal in te luiden.
+                Het toernooi wordt afgesloten met een barbecue en aansluitend een feest.
+                Dat wil je natuurlijk niet missen!
+            </p>
+
+            <p>
+                Het toernooi zal rond 12 uur starten (specifieke tijd volgt), de barbecue start om half 7.
+                Wil je alleen naar het feest komen, dan heb je <span class="underline">geen ticket</span> nodig en kun je gezellig binnenlopen.
+            </p>
+
+            <p>
+                Voor veelgestelde vragen en locatie bekijk de <a href="#/org" class="text-primary underline">organisatie</a> pagina
+                of stuur een mail naar <a href="mailto:toernooi.dasdelft@gmail.com?subject=Vragen%20familietoernooi%202022" class="text-primary underline">toernooi.dasdelft@gmail.com</a>.
+            </p>
+
+            <h2 class="mt-5 text-[8vw] leading-none mi:text-2xl">Dans mee op het toernooi</h2>
+            <p>
+                Na de BBQ kunnen de beentjes van de vloer met de SKA/Reggae band de Limelites van onze eigen Günter.
+                Met deze vrolijke muziek begint dan ook meteen het feest.
+                Zo rond 22:00 zal onze Ben met zijn DJ team de rest van de avond aan elkaar draaien.
+                Lekker met z&#39;n allen feestend de dag uit.
+            </p>
+
+            <p>
+                Het mooie van de nieuwe locatie is dat er geen bebouwing is, dus je bent vrij om naar buiten te gaan en het feest kan lekker lang doorgaan.
+                We maken er een mooi begin van!
+            </p>
         </div>
     </div>
 </main>
