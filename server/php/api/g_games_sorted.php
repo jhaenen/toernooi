7329c6c8c24acb2f3bd8d8b2d7edd4b136373451
jhaenen <jasper@haenenweb.nl>
2022-08-27 13:56:02 +0200

    Improve schema UI

---
+++ server/php/api/g_games_sorted.php
@@ -0,0 +1,82 @@
+<?php
+    // Return method not allowed if not GET
+    if ($_SERVER['REQUEST_METHOD'] != 'GET') {
+        header('HTTP/1.0 405 Method Not Allowed');
+        die();
+    }
+
+    $ini = parse_ini_file("../env.ini");
+
+    $host = $ini["DB_HOST"];
+    $user = $ini["DB_USER"];
+    $pass = $ini["DB_PASS"];
+    $db = $ini["DB_NAME"];
+
+    $conn = new mysqli($host, $user, $pass, $db);
+    
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+
+    $sql = "SELECT * FROM toernooi_wed_name ORDER BY time ASC, court_num ASC; ";
+
+    $result = $conn->query($sql);
+
+    $groups = array();
+
+    $curTime = null;
+    $groupIndex = 0;
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            
+            if ($row["time"] != $curTime) {
+                $curTime = $row["time"];
+
+                $groups[] = new \stdClass();
+                $groupIndex = count($groups) - 1;
+
+                $groups[$groupIndex]->time = $row["time"];
+                $groups[$groupIndex]->games = array();
+            }
+            
+            $groups[$groupIndex]->games[] = new \stdClass();
+            $gameIndex = count($groups[$groupIndex]->games) - 1;
+
+            $groups[$groupIndex]->games[$gameIndex]->id = intval($row["id"]);
+
+            $groups[$groupIndex]->games[$gameIndex]->poule = new \stdClass();
+            $groups[$groupIndex]->games[$gameIndex]->poule->id = intval($row["poule_id"]);
+            $groups[$groupIndex]->games[$gameIndex]->poule->name = $row["poule_name"];
+            $groups[$groupIndex]->games[$gameIndex]->poule->color = $row["poule_color"];
+
+            $groups[$groupIndex]->games[$gameIndex]->team1 = new \stdClass();
+            $groups[$groupIndex]->games[$gameIndex]->team1->id = intval($row["team1_id"]);
+            $groups[$groupIndex]->games[$gameIndex]->team1->name = $row["team1_name"];
+
+            $groups[$groupIndex]->games[$gameIndex]->team2 = new \stdClass();
+            $groups[$groupIndex]->games[$gameIndex]->team2->id = intval($row["team2_id"]);
+            $groups[$groupIndex]->games[$gameIndex]->team2->name = $row["team2_name"];
+
+            $groups[$groupIndex]->games[$gameIndex]->team1->score = intval($row["score1"]);
+            $groups[$groupIndex]->games[$gameIndex]->team2->score = intval($row["score2"]);
+
+            $groups[$groupIndex]->games[$gameIndex]->time = $row["time"];
+
+            $groups[$groupIndex]->games[$gameIndex]->court_num = $row["court_num"];
+
+            $groups[$groupIndex]->games[$gameIndex]->ref = new \stdClass();
+            $groups[$groupIndex]->games[$gameIndex]->ref->id = intval($row["ref_id"]);
+            $groups[$groupIndex]->games[$gameIndex]->ref->name = $row["ref_name"];
+
+        }
+    }
+
+    $conn->close();
+
+    // Set content type to json
+    header('Content-Type: application/json');
+
+    die(json_encode($groups));
+?>
--- src/routes/schema.svelte
+++ src/routes/schema.svelte
@@ -9,7 +9,12 @@
     import Result from "@/components/result.svelte";
     import Loader from "@/components/load-ball.svelte";
 
-    let games: Array<Game> = [];
+    interface TimeSlot {
+        time: string;
+        games: Array<Game>;
+    }
+
+    let gamesSorted: Array<TimeSlot> = [];
 
     let error = false;
 
@@ -18,8 +23,10 @@
 
         // Get games from server
         try {
-            const response = await fetch(server + "games");
-            games = await response.json();
+            const response = await fetch(server + "games/sorted");
+            gamesSorted = await response.json();
+
+            console.log(gamesSorted);
             setTimeout(scrollToCurrentGame, 500);
 
             error = false;
@@ -32,7 +39,7 @@
     });
 
     function scrollToCurrentGame() {
-        if (games.length == 0) return;
+        if (gamesSorted.length == 0) return;
 
         // const now = new Date(Date.now());
         const now = new Date(2022, 9, 10, 12, 23);
@@ -41,15 +48,15 @@
         
         let cur_diff = Number.MAX_VALUE;
 
-        for(const game of games) {
-            const time_arr = game.time.split(":");
+        for(const slot of gamesSorted) {
+            const time_arr = slot.time.split(":");
             const game_date = new Date(2022, 9, 10, parseInt(time_arr[0]), parseInt(time_arr[1]));
 
             const diff = Math.abs(game_date.getTime() - now.getTime());
             
             if(cur_diff > diff) {
                 cur_diff = diff;
-                currentGame = game;
+                currentGame = slot.games[0];
             }
         }
 
@@ -66,16 +73,24 @@
     }
 </script>
 
-<main>
-    <div class="flex flex-col items-center gap-y-5 m-4">
+<template>
+    <div class="flex flex-col items-center m-4">
         <!-- Title -->
         <h1 class="my-2 text-[12vw] leading-none font-thin mi:text-5xl">Dagschema</h1>
 
-        <!-- Game list -->
-        {#each games as game (game.id)}
-            <div id={"game" + game.id} class="scroll-mt-5"><Result team1={game.team1} team2={game.team2} time={game.time.substring(0, 5)} poule={game.poule.name} court_num={game.court_num} ref={game.ref} banner color={game.poule.color}/></div>
-        {:else}
-            <Loader {error}/>
-        {/each}
+        <div class="flex flex-col items-center w-fit">
+            <!-- Game list -->
+            {#each gamesSorted as slot (slot.time)}
+                <div class="border-b-2 border-slate-300 p-5 flex flex-col items-center justify-center gap-5 lg:flex-row lg:p-10 w-full flex-wrap">  
+                    {#each slot.games as game (game.id)}
+                        <div id={"game" + game.id} class="scroll-mt-5">
+                            <Result team1={game.team1} team2={game.team2} time={game.time.substring(0, 5)} poule={game.poule.name} court_num={game.court_num} ref={game.ref} banner color={game.poule.color}/>
+                        </div>
+                    {/each}
+                </div> 
+            {:else}
+                <Loader {error}/>
+            {/each}
+        </div>
     </div>
-</main>
+</template>
