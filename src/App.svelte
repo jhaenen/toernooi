1a6a5715383f7b52d5607088b21c18603e03a9bb
jhaenen <jasper@haenenweb.nl>
2022-08-30 13:54:47 +0200

    New logo

--- src/App.svelte
+++ src/App.svelte
@@ -8,7 +8,7 @@
     import Loader from '@/components/load-ball.svelte';
 
     // Assets
-    import logo from "@/assets/logo_lq.webp";
+    import logo from "@/assets/logo.svg";
     import home_icon from "@/assets/icons/house.svg";
     import poules_icon from "@/assets/icons/basketball.svg";
     import schedule_icon from "@/assets/icons/newspaper.svg";
@@ -17,11 +17,10 @@
 
     // Routes
     const routes = {
-        '/': wrap({asyncComponent: () => import('@/routes/home.svelte'), loadingComponent: Loader}),
-        '/schema': wrap({asyncComponent: () => import('@/routes/schema.svelte'), loadingComponent: Loader}),
+        '/': wrap({asyncComponent: () => import('@/routes/schema.svelte'), loadingComponent: Loader}),
         '/poules': wrap({asyncComponent: () => import('@/routes/poules.svelte'), loadingComponent: Loader}),
         '/poules/:id': wrap({asyncComponent: () => import('@/routes/poule_stats.svelte'), loadingComponent: Loader}),
-        '/org': wrap({asyncComponent: () => import('@/routes/org.svelte'), loadingComponent: Loader}),
+        '/info': wrap({asyncComponent: () => import('@/routes/info.svelte'), loadingComponent: Loader}),
         '*': wrap({asyncComponent: () => import('@/routes/404.svelte'), loadingComponent: Loader}),
     };
 
@@ -31,12 +30,12 @@
 <main>
     <!-- Navigation bar -->
     <div class="fixed bottom-0 w-full m-0 p-0 sm:top-0 sm:bottom-auto z-10">
-        <nav class="flex h-[82px] justify-evenly flex-wrap sm:justify-start bg-primary text-white py-4 sm:p3">
-            <a href="#/" class="hidden h-12 w-12 mx-4 sm:block"><img src={logo} alt="Svelte Logo"/></a>
+        <nav class="flex h-[82px] sm:h-[100px] justify-evenly flex-wrap sm:justify-start sm:items-center bg-primary text-white py-4 sm:p3">
+            <a href="#/" class="hidden h-16 w-16 mx-4 sm:block"><img src={logo} alt="Svelte Logo"/></a>
             <a href="#/" class="w-24 sm:py-3 sm:px-4 sm:w-auto"><NavButton label="Home" icon={home_icon} color={icon_color}/></a>
             <a href="#/poules" class="w-24 sm:py-3 sm:px-4 sm:w-auto"><NavButton label="Poules" icon={poules_icon} color={icon_color}/></a>
-            <a href="#/schema" class="w-24 sm:py-3 sm:px-4 sm:w-auto"><NavButton label="Dagschema" icon={schedule_icon} color={icon_color}/></a>
-            <a href="#/org" class="w-24 sm:py-3 sm:px-4 hidden mi:block sm:w-auto"><NavButton label="Organisatie" icon={org_icon} color={icon_color}/></a>
+            <a href="#/info" class="w-24 sm:py-3 sm:px-4 sm:w-auto"><NavButton label="Info" icon={schedule_icon} color={icon_color}/></a>
+            <!-- <a href="#/org" class="w-24 sm:py-3 sm:px-4 hidden mi:block sm:w-auto"><NavButton label="Organisatie" icon={org_icon} color={icon_color}/></a> -->
         </nav>
     </div>
     
--- src/routes/schema.svelte
+++ src/routes/schema.svelte
@@ -9,6 +9,9 @@
     import Result from "@/components/result.svelte";
     import Loader from "@/components/load-ball.svelte";
 
+    // Assets
+    import logo from "@/assets/logo.svg";
+
     interface TimeSlot {
         time: string;
         games: Array<Game>;
@@ -17,6 +20,7 @@
     let gamesSorted: Array<TimeSlot> = [];
 
     let error = false;
+    let loaded = false;
 
     onMount(async () => {
         const server = import.meta.env.VITE_SERVER_URL;
@@ -26,10 +30,10 @@
             const response = await fetch(server + "games/sorted");
             gamesSorted = await response.json();
 
-            console.log(gamesSorted);
             setTimeout(scrollToCurrentGame, 500);
 
             error = false;
+            loaded = true;
         } catch (err) {
             error = true;
             console.error(err);
@@ -41,8 +45,9 @@
     function scrollToCurrentGame() {
         if (gamesSorted.length == 0) return;
 
-        // const now = new Date(Date.now());
-        const now = new Date(2022, 9, 10, 12, 23);
+        const now = new Date(Date.now());
+        // const now = new Date(2022, 9, 10, 12, 23);
+        // log now
 
         let currentGame: Game;
         
@@ -50,7 +55,7 @@
 
         for(const slot of gamesSorted) {
             const time_arr = slot.time.split(":");
-            const game_date = new Date(2022, 9, 10, parseInt(time_arr[0]), parseInt(time_arr[1]));
+            const game_date = new Date(2022, 8, 10, parseInt(time_arr[0]), parseInt(time_arr[1]));
 
             const diff = Math.abs(game_date.getTime() - now.getTime());
             
@@ -75,21 +80,30 @@
 
 <template>
     <div class="flex flex-col items-center m-4">
+        <!-- Logo -->
+        <img class="w-[40%] h-auto max-w-xs sm:hidden" src={logo} alt="DAS logo" width="320" height="320"/>
+        
         <!-- Title -->
-        <h1 class="my-2 text-[12vw] leading-none font-thin mi:text-5xl">Dagschema</h1>
+        <h1 class="my-2 text-[12vw] leading-none font-thin mi:text-5xl sm:mt-8">Dagschema</h1>
 
+        <!-- Game list -->
         <div class="flex flex-col items-center w-fit">
-            <!-- Game list -->
             {#each gamesSorted as slot (slot.time)}
                 <div class="border-b-2 border-slate-300 p-5 flex flex-col items-center justify-center gap-5 lg:flex-row lg:p-10 w-full flex-wrap">  
                     {#each slot.games as game (game.id)}
-                        <div id={"game" + game.id} class="scroll-mt-5">
+                        <div id={"game" + game.id} class="scroll-mt-5 sm:scroll-mt-10">
                             <Result team1={game.team1} team2={game.team2} time={game.time.substring(0, 5)} poule={game.poule.name} court_num={game.court_num} ref={game.ref} banner color={game.poule.color}/>
                         </div>
                     {/each}
                 </div> 
             {:else}
-                <Loader {error}/>
+                {#if !loaded} 
+                    <Loader {error}/>
+                {:else}
+                    <div class="text-center">
+                        <p>Er zijn geen wedstrijden gepland.</p>
+                    </div>
+                {/if}
             {/each}
         </div>
     </div>
