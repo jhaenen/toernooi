37c668b1803aa4bb3e51e3e859ef4d312d0af812
jhaenen <jasper@haenenweb.nl>
2022-09-02 12:23:56 +0200

    Implement CORS

--- server/php/api/g_games_sorted.php
+++ server/php/api/g_games_sorted.php
@@ -1,4 +1,13 @@
 <?php
+    // Allow from any origin
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+        // you want to allow, and if so:
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    }
+
     // Return method not allowed if not GET
     if ($_SERVER['REQUEST_METHOD'] != 'GET') {
         header('HTTP/1.0 405 Method Not Allowed');
