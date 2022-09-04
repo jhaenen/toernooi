bfba86f755a7742b9e4c1c3e9e0d174dd59ae0ff
jhaenen <jasper@haenenweb.nl>
2022-09-04 21:45:27 +0200

    Add rules

--- src/routes/info.svelte
+++ src/routes/info.svelte
@@ -1,3 +1,7 @@
+<script>
+    import Rules from "@/components/rules.svelte";
+
+</script>
 <main>
     <div class="flex flex-col items-center sm:items-start sm:text-left">
         <!-- Title -->
@@ -6,24 +10,25 @@
         <!-- Main text -->
         <div class="flex flex-col m-3 gap-4">
             <!-- FAQ -->
-            <h2 class="mt-5 text-[8vw] leading-none sm:text-2xl">Veelgestelde vragen</h2>
+            <h2 class="mt-5 text-[8vw] leading-none mi:text-3xl">Veelgestelde vragen</h2>
             <div>TODO...</div>
 
             <!-- Reglement -->
-            <h2 class="mt-5 text-[8vw] leading-none sm:text-2xl">Reglement</h2>
-            <div>TODO...</div>
+            <h2 id="rules" class="mt-5 text-[8vw] leading-none mi:text-3xl scroll-mt-2">Reglement</h2>
+            <div><Rules/></div>
 
             <!-- Address -->
-            <h2 class="mt-5 text-[8vw] leading-none sm:text-2xl">Adres</h2>
+            <h2 class="mt-5 text-[8vw] leading-none mi:text-3xl">Adres</h2>
             <a href="https://goo.gl/maps/BDXNNJDC43LKQzZT8" target="_blank" class="text-primary underline"><address>Schoemakerstraat 340, 2629 HP Delft</address></a>
             {#if navigator.onLine}
                 <iframe title="kaart" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d614.1854017026016!2d4.3821292!3d51.9933631!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5b60b00000001%3A0x95c80e12a0134cd5!2sBasketbalvereniging%20Das%20Delft!5e0!3m2!1snl!2snl!4v1661557009444!5m2!1snl!2snl" width="600" height="300" style="border:0;" allow="fullscreen" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="w-[80vw] h-80 sm:w-[600px]"></iframe>
             {/if}
 
             <!-- Contact -->
-            <h2 class="mt-5 text-[8vw] leading-none sm:text-2xl">Contact</h2>
+            <h2 class="mt-5 text-[8vw] leading-none mi:text-3xl">Contact</h2>
             <div>E-mail: <a href="mailto:toernooi.dasdelft@gmail.com?subject=Vragen%20familietoernooi%202022" class="text-primary underline">toernooi.dasdelft@gmail.com</a></div>
 
+            <div class="h-5"></div>
             
         </div>
     </div>
