fbbd07761b81d0575099e71fb12b476948783a44
jhaenen <jasper@haenenweb.nl>
2024-06-14 14:42:48 +0200

    edit rules

--- src/components/rules.svelte
+++ src/components/rules.svelte
@@ -1,59 +1,131 @@
 <script lang="ts">
-    let preview = true;
+  let preview = true;
 
-    function togglePreview() {
-        preview = !preview;
+  function togglePreview() {
+    preview = !preview;
 
-        const header_el = document.getElementById("rules");
-        const bounding = header_el.getBoundingClientRect();
+    const header_el = document.getElementById("rules");
+    const bounding = header_el.getBoundingClientRect();
 
-        if(!(bounding.top >= 0 && bounding.left >= 0 && bounding.right <= window.innerWidth && bounding.bottom <= window.innerHeight)) {
-            if (header_el) {
-                header_el.scrollIntoView({ behavior: "smooth" });
-            } else {
-                console.error("Could not find header of rules");
-            }
-        }
+    if (
+      !(
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.right <= window.innerWidth &&
+        bounding.bottom <= window.innerHeight
+      )
+    ) {
+      if (header_el) {
+        header_el.scrollIntoView({ behavior: "smooth" });
+      } else {
+        console.error("Could not find header of rules");
+      }
     }
+  }
 </script>
 
 <template>
-    <div>
-        <div class="overflow-hidden relative transition-[max-height] duration-300" class:max-h-64={preview} class:max-h-[1848px]={!preview}>
-            <p>De wedstrijden worden gespeeld volgens de reglementen van de Nederlandse Basketball Bond, waarbij onderstaande afwijkende regels gelden:</p>
-            <ol class="list-decimal list-inside my-2 mx-4">
-                <li class="my-2">De wedstrijden duren 13 minuten doorlopende tijd. Na de wedstrijd is er 2 minuten tijd voor wisselen. (dus zorg dat je op tijd klaar staat met je hele team)</li>
-                <li class="my-2">De speeltijd is doorlopend en wordt centraal geregeld door de wedstrijdleiding. De wedstrijd is afgelopen na het signaal van de wedstrijdleiding en niet van de scheidsrechter!</li>
-                <li class="my-2">Een fout op een doelende speler zonder score: geen vrije worp maar + 1 punt en het aanvallende team houdt de bal en neemt de bal in bij de achterlijn.</li>
-                <li class="my-2">Bij een fout op een doelende speler mét score: géén (bonus)vrije worpen, wel + 1
-                punt (dus 3 of 4 in totaal). De verdedigende partij neemt daarna de bal in.</li>
-                <li class="my-2">Als een team 6 teamfouten heeft krijgt de tegenstander bij elk volgende fout + 1 punt en balbezit. (Dit geldt niet voor aanvallende fouten)</li>
-                <li class="my-2">Wissels mogen alleen bij een dood spelmoment en moet via de scheidsrechter worden aangevraagd. De laatste minuut mag er niet meer gewisseld worden.</li>
-                <li class="my-2">Winnaar krijgt 2 punten, bij gelijkspel ieder 1 punt, verliezer 0 punten</li>
-                <li class="my-2">Eindstanden in de poule: Indien 2 teams gelijk eindigen in de poule is de in navolgende volgorde de beslissing:
-                    <ol class="list-[lower-alpha] list-inside my-2 mx-4">
-                        <li class="my-2">het doelsaldo van alle wedstrijden; wanneer deze ook gelijk is…</li>
-                        <li class="my-2">het aantal gescoorde doelpunten; wanneer deze ook gelijk is...</li>
-                        <li class="my-2">een serie van 3 vrije worpen door verschillende spelers/speelsters uit een team.</li>
-                    </ol>
-                </li>
-                <li class="my-2">In geval van meningsverschillen of omissies beslist de wedstrijdleiding.</li>
-                <li class="my-2">Protesten? Zijn niet nodig en waar dit wel nodig is, treedt regel 9 in werking!</li>
-                <p class="my-2 -mx-4">Extra regels mini's:</p>
-                <li class="my-2">De bal mag bij een schotpoging niet geblockt worden.</li>
-                <li class="my-2">De bal mag niet bij een andere speler uit zijn handen worden getrokken.</li>
-                <li class="my-2">Alle scores tellen voor 2 punten. (Er zijn dus geen 3 punters mogelijk)</li>
-            </ol>
+  <div>
+    <div
+      class="overflow-hidden relative transition-[max-height] duration-300"
+      class:max-h-64={preview}
+      class:max-h-[1848px]={!preview}
+    >
+      <p>
+        De wedstrijden worden gespeeld volgens de reglementen van de Nederlandse
+        Basketball Bond, waarbij onderstaande afwijkende regels gelden:
+      </p>
+      <ol class="list-decimal list-inside my-2 mx-4">
+        <li class="my-2">
+          De wedstrijden duren 15 minuten doorlopende tijd. Na de wedstrijd is
+          er 5 minuten tijd voor wisselen, in deze tijd kan je ook nog even met
+          je team warmlopen. (zorg dat je op tijd klaar staat met je hele team)
+        </li>
+        <li class="my-2">
+          De speeltijd is doorlopend en wordt centraal geregeld door de
+          wedstrijdleiding. De wedstrijd is afgelopen na het signaal van de
+          wedstrijdleiding en niet van de scheidsrechter!
+        </li>
+        <li class="my-2">
+          Een fout op een doelende speler zonder score: geen vrije worp maar + 1
+          punt en het aanvallende team houdt de bal en neemt de bal in bij de
+          achterlijn.
+        </li>
+        <li class="my-2">
+          Bij een fout op een doelende speler mét score: géén (bonus)vrije
+          worpen, wel + 1 punt (dus 3 of 4 in totaal). De verdedigende partij
+          neemt daarna de bal in.
+        </li>
+        <li class="my-2">
+          Als een team 6 teamfouten heeft krijgt de tegenstander bij elk
+          volgende fout + 1 punt en balbezit. (Dit geldt niet voor aanvallende
+          fouten)
+        </li>
+        <li class="my-2">
+          Wissels mogen alleen bij een dood spelmoment en moet via de
+          scheidsrechter worden aangevraagd. De laatste minuut mag er niet meer
+          gewisseld worden.
+        </li>
+        <li class="my-2">
+          Winnaar krijgt 2 punten, bij gelijkspel ieder 1 punt, verliezer 0
+          punten
+        </li>
+        <li class="my-2">
+          Eindstanden in de poule: Indien 2 teams gelijk eindigen in de poule is
+          de in navolgende volgorde de beslissing:
+          <ol class="list-[lower-alpha] list-inside my-2 mx-4">
+            <li class="my-2">
+              het doelsaldo van alle wedstrijden; wanneer deze ook gelijk is…
+            </li>
+            <li class="my-2">
+              het aantal gescoorde doelpunten; wanneer deze ook gelijk is...
+            </li>
+            <li class="my-2">
+              een serie van 3 vrije worpen door verschillende spelers/speelsters
+              uit een team.
+            </li>
+          </ol>
+        </li>
+        <li class="my-2">
+          In geval van meningsverschillen of omissies beslist de
+          wedstrijdleiding.
+        </li>
+        <li class="my-2">
+          Protesten? Zijn niet nodig en waar dit wel nodig is, treedt regel 9 in
+          werking!
+        </li>
+        <p class="my-2 -mx-4">Extra regels mini's:</p>
+        <li class="my-2">
+          De bal mag bij een schotpoging niet geblockt worden.
+        </li>
+        <li class="my-2">
+          De bal mag niet bij een andere speler uit zijn handen worden
+          getrokken.
+        </li>
+        <li class="my-2">
+          Alle scores tellen voor 2 punten. (Er zijn dus geen 3 punters
+          mogelijk)
+        </li>
+      </ol>
 
-            <div class="absolute bottom-0 left-0 w-full text-center m-0 h-3/4 z-[5] transition-opacity duration-300" class:opacity-0={!preview} class:opacity-1={preview} style="background-image: linear-gradient(to bottom, rgba(255,255,255,0), rgba(255, 255, 255, 1));"></div>
-        </div>
-        <div class="w-full flex justify-center mt-3"><button class="bg-primary text-white px-4 py-2 rounded-full" on:click={togglePreview}>
-                {#if preview}
-                    Lees meer
-                {:else}
-                    Verberg tekst
-                {/if}
-            </button>
-        </div>
+      <div
+        class="absolute bottom-0 left-0 w-full text-center m-0 h-3/4 z-[5] transition-opacity duration-300"
+        class:opacity-0={!preview}
+        class:opacity-1={preview}
+        style="background-image: linear-gradient(to bottom, rgba(255,255,255,0), rgba(255, 255, 255, 1));"
+      ></div>
     </div>
+    <div class="w-full flex justify-center mt-3">
+      <button
+        class="bg-primary text-white px-4 py-2 rounded-full"
+        on:click={togglePreview}
+      >
+        {#if preview}
+          Lees meer
+        {:else}
+          Verberg tekst
+        {/if}
+      </button>
+    </div>
+  </div>
 </template>
--- src/routes/info.svelte
+++ src/routes/info.svelte
@@ -1,40 +1,72 @@
 <script>
-    import Rules from "@/components/rules.svelte";
-    import Schedule from "@/components/schedule.svelte";
-
+  import Rules from "@/components/rules.svelte";
+  import Schedule from "@/components/schedule.svelte";
 </script>
+
 <main>
-    <div class="flex flex-col items-center sm:items-start sm:text-left overflow-hidden">
-        <!-- Title -->
-        <h1 class="m-4 text-[12vw] w-full text-center leading-none font-light mi:text-5xl sm:mt-8 sm:text-left">Algemene informatie</h1>
+  <div
+    class="flex flex-col items-center sm:items-start sm:text-left overflow-hidden"
+  >
+    <!-- Title -->
+    <h1
+      class="m-4 text-[12vw] w-full text-center leading-none font-light mi:text-5xl sm:mt-8 sm:text-left"
+    >
+      Algemene informatie
+    </h1>
 
-        <!-- Main text -->
-        <div class="flex flex-col mt-3 mx-3 gap-4">
-            <!-- FAQ -->
+    <!-- Main text -->
+    <div class="flex flex-col mt-3 mx-3 gap-4">
+      <!-- FAQ
             <h2 class="mt-5 text-[8vw] leading-none mi:text-3xl">Dagschema</h2>
-            <div><Schedule/></div>
+            <div><Schedule/></div> -->
 
-            <!-- Reglement -->
-            <h2 id="rules" class="mt-5 text-[8vw] leading-none mi:text-3xl scroll-mt-2">Reglement</h2>
-            <div><Rules/></div>
+      <!-- Reglement -->
+      <h2
+        id="rules"
+        class="mt-5 text-[8vw] leading-none mi:text-3xl scroll-mt-2"
+      >
+        Reglement
+      </h2>
+      <div><Rules /></div>
 
-            <!-- Address -->
-            <h2 class="mt-5 text-[8vw] leading-none mi:text-3xl">Adres</h2>
-            <a href="https://goo.gl/maps/BDXNNJDC43LKQzZT8" target="_blank" class="text-primary underline"><address>Schoemakerstraat 340, 2629 HP Delft</address></a>
-            {#if navigator.onLine}
-                <iframe title="kaart" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d614.1854017026016!2d4.3821292!3d51.9933631!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5b60b00000001%3A0x95c80e12a0134cd5!2sBasketbalvereniging%20Das%20Delft!5e0!3m2!1snl!2snl!4v1661557009444!5m2!1snl!2snl" width="600" height="300" style="border:0;" allow="fullscreen" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="w-[80vw] h-80 sm:w-[600px]"></iframe>
-            {/if}
+      <!-- Address -->
+      <h2 class="mt-5 text-[8vw] leading-none mi:text-3xl">Adres</h2>
+      <a
+        href="https://goo.gl/maps/BDXNNJDC43LKQzZT8"
+        target="_blank"
+        class="text-primary underline"
+        ><address>Schoemakerstraat 340, 2629 HP Delft</address></a
+      >
+      {#if navigator.onLine}
+        <iframe
+          title="kaart"
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d614.1854017026016!2d4.3821292!3d51.9933631!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5b60b00000001%3A0x95c80e12a0134cd5!2sBasketbalvereniging%20Das%20Delft!5e0!3m2!1snl!2snl!4v1661557009444!5m2!1snl!2snl"
+          width="600"
+          height="300"
+          style="border:0;"
+          allow="fullscreen"
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+          class="w-[80vw] h-80 sm:w-[600px]"
+        ></iframe>
+      {/if}
 
-            <!-- Contact -->
-            <h2 class="mt-5 text-[8vw] leading-none mi:text-3xl">Contact</h2>
-            <div>E-mail: <a href="mailto:toernooi.dasdelft@gmail.com?subject=Vragen%20familietoernooi%202022" class="text-primary underline">toernooi.dasdelft@gmail.com</a></div>
-
-            
-
-        </div>
-
-        <!-- Acknowledgements -->
-        <div class="w-full text-center text-sm text-slate-800 border-t-2 pt-2 mt-8 sm:mt-5">Ontwikkeld en ontworpen door: Jasper Haenen</div>
-        <div class="sm:h-5"></div>
+      <!-- Contact -->
+      <h2 class="mt-5 text-[8vw] leading-none mi:text-3xl">Contact</h2>
+      <div>
+        E-mail: <a
+          href="mailto:toernooi.dasdelft@gmail.com?subject=Vragen%20familietoernooi%202022"
+          class="text-primary underline">toernooi.dasdelft@gmail.com</a
+        >
+      </div>
     </div>
+
+    <!-- Acknowledgements -->
+    <div
+      class="w-full text-center text-sm text-slate-800 border-t-2 pt-2 mt-8 sm:mt-5"
+    >
+      Ontwikkeld en ontworpen door: Jasper Haenen
+    </div>
+    <div class="sm:h-5"></div>
+  </div>
 </main>
